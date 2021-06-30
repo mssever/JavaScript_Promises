6bb43a02383809239c6f6dc10393e2b5432a3438
Scott Severance <scott@scottseverance.us>
2021-06-29 21:24:35 -0500

    beginning work

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-console.log("Hello World!\n==========\n");
+'use strict';
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function watchTurorialCallback(callback, errorCallback) {
+  let userLeft = false;
+  let userWatchingLiveStream = true;
+
+  if (userLeft) {
+    errorCallback({
+      name: "User Left",
+      message: ":(",
+    });
+  } else if (userWatchingLiveStream) {
+    callback("Thumbs up and Subscribe");
+  }
+}
+
+watchTurorialCallback(
+  (message) => {
+    console.log(message);
+  },
+  (error) => {
+    console.log(error.name + " " + error.message);
+  }
+);
